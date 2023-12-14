1b1e1c7dc5a95f207c2e3f97df064880342ae961
DPlauder <docgit@gmail.com>
2023-12-14 11:42:28 +0100

    fixed bug

--- sortieralgorythmen/bubblesort/bubblesort.js
+++ sortieralgorythmen/bubblesort/bubblesort.js
@@ -1,18 +1,20 @@
 "use strict";
 const swap = (myArr, index1, index2) => {
     const numBackup = myArr[index1];
+    console.log("ind1= ", index1, " ind2= ", index2);
     myArr[index1] = myArr[index2];
     myArr[index2] = numBackup;
-    return myArr;
 };
 const bubbleSort = (myArr) => {
-    for (let i = 0; i < myArr.length; i++) {
+    for (let i = 0; i < myArr.length - 1; i++) {
         for (let j = 0; j < myArr.length - 1; j++) {
-            if (myArr[j] > myArr[j + 1])
-                myArr = swap(myArr, i, j);
+            if (myArr[j] > myArr[j + 1]) {
+                console.log(myArr[j], " > ", myArr[j + 1]);
+                swap(myArr, j, j + 1);
+            }
         }
     }
     return myArr;
 };
-console.log(bubbleSort([23, 10, 14, 37, 24]));
+console.log(bubbleSort([23, 10, 11, 200, 14, 100, 37]));
 //# sourceMappingURL=bubblesort.js.map
