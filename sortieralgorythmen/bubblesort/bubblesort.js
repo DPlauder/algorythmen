d4bf6117a6d4588da252b2f2a3100336ab4b383b
DPlauder <docgit@gmail.com>
2023-12-14 12:25:22 +0100

    create abbruch wenn kein wechsel mehr

--- sortieralgorythmen/bubblesort/bubblesort.js
+++ sortieralgorythmen/bubblesort/bubblesort.js
@@ -1,20 +1,23 @@
 "use strict";
 const swap = (myArr, index1, index2) => {
     const numBackup = myArr[index1];
-    console.log("ind1= ", index1, " ind2= ", index2);
     myArr[index1] = myArr[index2];
     myArr[index2] = numBackup;
 };
 const bubbleSort = (myArr) => {
     for (let i = 0; i < myArr.length - 1; i++) {
+        let change = false;
         for (let j = 0; j < myArr.length - 1; j++) {
+            console.log(myArr[j]);
             if (myArr[j] > myArr[j + 1]) {
-                console.log(myArr[j], " > ", myArr[j + 1]);
                 swap(myArr, j, j + 1);
+                change = true;
             }
         }
+        if (!change)
+            break;
     }
     return myArr;
 };
-console.log(bubbleSort([23, 10, 11, 200, 14, 100, 37]));
+console.log(bubbleSort([1, 200, 23, 12, 13, 300, 10, 11, 200, 14, 100, 37]));
 //# sourceMappingURL=bubblesort.js.map
--- sortieralgorythmen/bubblesort/bubblesort.ts
+++ sortieralgorythmen/bubblesort/bubblesort.ts
@@ -1,20 +1,22 @@
 const swap = (myArr: number[], index1: number, index2: number) => {
   const numBackup = myArr[index1];
-  console.log("ind1= ", index1, " ind2= ", index2);
   myArr[index1] = myArr[index2];
   myArr[index2] = numBackup;
 };
 
 const bubbleSort = (myArr: number[]): number[] => {
   for (let i = 0; i < myArr.length - 1; i++) {
+    let change = false;
     for (let j = 0; j < myArr.length - 1; j++) {
+      console.log(myArr[j]);
       if (myArr[j] > myArr[j + 1]) {
-        console.log(myArr[j], " > ", myArr[j + 1]);
         swap(myArr, j, j + 1);
+        change = true;
       }
     }
+    if (!change) break;
   }
   return myArr;
 };
 
-console.log(bubbleSort([23, 10, 11, 200, 14, 100, 37]));
+console.log(bubbleSort([1, 200, 23, 12, 13, 300, 10, 11, 200, 14, 100, 37]));
