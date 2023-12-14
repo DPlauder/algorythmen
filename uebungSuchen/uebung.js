d2ca2cb58012256cbb9821cf82b6a2b33d9f2a0b
Astromin21 <docgit@gmail.com>
2023-12-14 09:28:47 +0100

    git init

---
+++ uebungSuchen/uebung.js
@@ -0,0 +1,47 @@
+"use strict";
+//lineare Suche
+/*
+const linearSearch = (arr: number[], numb: number): number =>{
+    for(let i = 0; i < arr.length; i++){
+        if(arr[i] === numb) return i;
+    }
+    return -1
+}
+console.log(linearSearch([1,2,5,7,8,10,9,6], 7)); // 3
+console.log(linearSearch([1,2,5,7,8,10,9,6], 3)); // -1
+
+//Binäre Suche
+const banarySearch = (arr: number[], value: number) => {
+  let left = arr[0];
+  let right = arr.length;
+  let mid = Math.floor((left + right) / 2);
+
+  if (value > mid) left = mid;
+  else if (value < mid) right = mid;
+
+  for (let i = left; i < right; i++) {
+    if (arr[i] === value) return i;
+  }
+  return -1;
+};
+console.log(banarySearch([1, 2, 3, 4, 5, 6, 7, 8, 9], 5));
+ */
+// Substing Suche
+const searchSubString = (string1, string2) => {
+    let counter = 0;
+    for (let i = 0; i < string1.length; i++) {
+        for (let j = 0; j < string2.length; j++) {
+            if (string1[i + j] === string2[j]) {
+                if (j === string2.length - 1)
+                    counter += 1;
+            }
+            else
+                break;
+        }
+    }
+    if (counter)
+        return counter;
+    return -1;
+};
+console.log(searchSubString("bizz bumm bam bang bing bam", "bam"));
+//# sourceMappingURL=uebung.js.map
---
+++ wiederholung/wiederholung.ts
@@ -0,0 +1,41 @@
+/* 
+function reverseString(text: string): string {
+    if(text === "") return "";
+    return reverseString(text.substring(1)) + text.charAt(0);
+}
+
+//Code Performance mot Big O
+const reverseString2 = (text:string) => {return text.split("").reverse().join("")}
+
+const reverseString3 = (text: string): string =>{return [...text].reverse().join("")}
+
+
+const addUpUpto = (nums: number): number =>{
+    let total = 0;
+    for(let i = 0;i <= nums; i++){
+        total += 1;
+    }
+    return total;
+}
+addUpUpto(6);
+//
+const addUpToGauss = (num: number): number =>{
+    return (num* (num + 1) / 2);
+}
+let time1 = performance.now();
+addUpUpto(10);
+//addUpToGauss(10);
+let time2 = performance.now();
+
+console.log(`Time Elapsed: ${(time2 - time1) / 1000} seconds `);
+ */
+
+const linearSearch = (arr: number[], numb: number): number =>{
+    for(let i = 0; i < arr.length; i++){
+        if(arr[i] === numb) return i;
+    }
+    return -1
+}
+console.log(linearSearch([1,2,5,7,8,10,9,6], 7)); // 3
+console.log(linearSearch([1,2,5,7,8,10,9,6], 3)); // -1
+
