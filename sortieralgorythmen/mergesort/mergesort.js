84c30b57e38c9a988a99194f9b4ae745808f8b60
DPlauder <docgit@gmail.com>
2023-12-15 10:53:11 +0100

    mergesort array eintrag update

--- sortieralgorythmen/mergesort/mergesort.js
+++ sortieralgorythmen/mergesort/mergesort.js
@@ -5,22 +5,22 @@ const merge = (myArr1, myArr2) => {
     let indexArr2 = 0;
     while (myArr1.length > indexArr1 && myArr2.length > indexArr2) {
         if (myArr1[indexArr1] < myArr2[indexArr2]) {
-            mySortedArray.push(myArr1[indexArr1]);
+            mySortedArray[indexArr1 + indexArr2] = myArr1[indexArr1];
             indexArr1++;
         }
         else if (myArr1[indexArr1] > myArr2[indexArr2]) {
-            mySortedArray.push(myArr2[indexArr2]);
+            mySortedArray[indexArr1 + indexArr2] = myArr2[indexArr2];
             indexArr2++;
         }
     }
     if (myArr1.length > indexArr1) {
         for (let i = indexArr1; i < myArr1.length; i++) {
-            mySortedArray.push(myArr1[i]);
+            mySortedArray[i + indexArr2] = myArr1[i];
         }
     }
     if (myArr2.length > indexArr2) {
         for (let i = indexArr2; i < myArr2.length; i++) {
-            mySortedArray.push(myArr2[i]);
+            mySortedArray[i + indexArr1] = myArr2[i];
         }
     }
     return mySortedArray;
--- sortieralgorythmen/mergesort/mergesort.ts
+++ sortieralgorythmen/mergesort/mergesort.ts
@@ -4,21 +4,21 @@ const merge = (myArr1: number[], myArr2: number[]) => {
   let indexArr2: number = 0;
   while (myArr1.length > indexArr1 && myArr2.length > indexArr2) {
     if (myArr1[indexArr1] < myArr2[indexArr2]) {
-      mySortedArray.push(myArr1[indexArr1]);
+      mySortedArray[indexArr1 + indexArr2] = myArr1[indexArr1];
       indexArr1++;
     } else if (myArr1[indexArr1] > myArr2[indexArr2]) {
-      mySortedArray.push(myArr2[indexArr2]);
+      mySortedArray[indexArr1 + indexArr2] = myArr2[indexArr2];
       indexArr2++;
     }
   }
   if (myArr1.length > indexArr1) {
     for (let i = indexArr1; i < myArr1.length; i++) {
-      mySortedArray.push(myArr1[i]);
+      mySortedArray[i + indexArr2] = myArr1[i];
     }
   }
   if (myArr2.length > indexArr2) {
     for (let i = indexArr2; i < myArr2.length; i++) {
-      mySortedArray.push(myArr2[i]);
+      mySortedArray[i + indexArr1] = myArr2[i];
     }
   }
   return mySortedArray;
