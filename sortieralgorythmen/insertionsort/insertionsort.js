3b89430fe7d02369b89c4912a982fec457fa77ee
DPlauder <docgit@gmail.com>
2023-12-15 08:38:29 +0100

    create insertionsort

---
+++ sortieralgorythmen/insertionsort/insertionsort.js
@@ -0,0 +1,20 @@
+"use strict";
+const insertionSort = (myArr) => {
+    let checkNum;
+    let j;
+    for (let i = 1; i < myArr.length; i++) {
+        checkNum = myArr[i];
+        console.log("checknum =", checkNum);
+        for (j = i - 1; j >= 0; j--) {
+            console.log("with =", myArr[j]);
+            if (myArr[j + 1] < myArr[j]) {
+                myArr[j + 1] = myArr[j];
+                myArr[j] = checkNum;
+            }
+        }
+        console.log(myArr);
+    }
+    return myArr;
+};
+console.log(insertionSort([200, 23, 1, 12, 13, 300, 10, 11, 14, 100, 37]));
+//# sourceMappingURL=insertionsort.js.map
---
+++ sortieralgorythmen/insertionsort/insertionsort.ts
@@ -0,0 +1,20 @@
+const insertionSort = (myArr: number[]) => {
+  let checkNum: number;
+  let j: number;
+  for (let i = 1; i < myArr.length; i++) {
+    checkNum = myArr[i];
+    console.log("checknum =", checkNum);
+    for (j = i - 1; j >= 0; j--) {
+      console.log("with =", myArr[j]);
+
+      if (myArr[j + 1] < myArr[j]) {
+        myArr[j + 1] = myArr[j];
+        myArr[j] = checkNum;
+      }
+    }
+    console.log(myArr);
+  }
+  return myArr;
+};
+
+console.log(insertionSort([200, 23, 1, 12, 13, 300, 10, 11, 14, 100, 37]));
