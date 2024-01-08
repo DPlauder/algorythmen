64b677219737ec53281f3bb0c3cc4481130e07b8
Dominik <docgit@gmail.comgit commit -m first>
2024-01-08 10:55:32 +0100

    create find

--- data_structur/Tree/tree.js
+++ data_structur/Tree/tree.js
@@ -22,6 +22,54 @@ class Tree {
         node.right = this.buildTree(data, mid + 1, end);
         return (this.root = node);
     }
+    insert(value) {
+        let newNode = new TreeNode(value);
+        if (!this.root)
+            return (this.root = newNode);
+        let current = this.root;
+        while (true) {
+            if (value === current.value)
+                return;
+            if (value < current.value) {
+                if (!current.left)
+                    return (current.left = newNode);
+                current = current.left;
+            }
+            if (value > current.value) {
+                if (!current.right)
+                    return (current.right = newNode);
+                current = current.right;
+            }
+        }
+    }
+    insertRec(value) {
+        var _a;
+        const check = (node) => {
+            var _a, _b;
+            if (node.value === value)
+                return;
+            if (node.value > value) {
+                check((node.left = (_a = node.left) !== null && _a !== void 0 ? _a : new TreeNode(value)));
+            }
+            if (node.value < value) {
+                check((node.right = (_b = node.right) !== null && _b !== void 0 ? _b : new TreeNode(value)));
+            }
+        };
+        check((this.root = (_a = this.root) !== null && _a !== void 0 ? _a : new TreeNode(value)));
+    }
+    find(value) {
+        var _a;
+        //if (!this.root) return;
+        const search = (node, value) => {
+            var _a;
+            if (node === null)
+                return;
+            if (node.value === value)
+                return node;
+            return (_a = search(node.left, value)) !== null && _a !== void 0 ? _a : search(node.right, value);
+        };
+        return (_a = search(this.root, value)) !== null && _a !== void 0 ? _a : -1;
+    }
 }
 const prettyPrint = (node, prefix = "", isLeft = true) => {
     if (node.right !== null) {
@@ -32,9 +80,13 @@ const prettyPrint = (node, prefix = "", isLeft = true) => {
         prettyPrint(node.left, `${prefix}${isLeft ? "│   " : "    "}`, true);
     }
 };
-const data = [5, 10, 21, 87, 301, 350];
+//const data = [5, 10, 21, 87, 301, 350];
 const tree = new Tree();
-tree.buildTree(data);
+tree.insertRec(5);
+tree.insertRec(6);
+tree.insertRec(2);
+tree.insertRec(3);
+console.log(tree.find(6));
+//tree.buildTree(data);
 prettyPrint(tree.root);
-console.log(tree);
 //# sourceMappingURL=tree.js.map
--- data_structur/Tree/tree.ts
+++ data_structur/Tree/tree.ts
@@ -20,6 +20,43 @@ class Tree<T> {
     node.right = this.buildTree(data, mid + 1, end);
     return (this.root = node);
   }
+  insert(value: T) {
+    let newNode = new TreeNode<T>(value);
+    if (!this.root) return (this.root = newNode);
+    let current = this.root;
+    while (true) {
+      if (value === current.value) return;
+      if (value < current.value) {
+        if (!current.left) return (current.left = newNode);
+        current = current.left;
+      }
+      if (value > current.value) {
+        if (!current.right) return (current.right = newNode);
+        current = current.right;
+      }
+    }
+  }
+  insertRec(value: T) {
+    const check = (node: TreeNode<T>) => {
+      if (node.value === value) return;
+      if (node.value > value) {
+        check((node.left = node.left ?? new TreeNode<T>(value)));
+      }
+      if (node.value < value) {
+        check((node.right = node.right ?? new TreeNode<T>(value)));
+      }
+    };
+    check((this.root = this.root ?? new TreeNode<T>(value)));
+  }
+  find(value: T) {
+    //if (!this.root) return;
+    const search: any = (node: TreeNode<T>, value: T) => {
+      if (node === null) return;
+      if (node.value === value) return node;
+      return search(node.left!, value) ?? search(node.right!, value);
+    };
+    return search(this.root, value) ?? -1;
+  }
 }
 const prettyPrint = (node: TreeNode<number>, prefix = "", isLeft = true) => {
   if (node.right !== null) {
@@ -31,8 +68,15 @@ const prettyPrint = (node: TreeNode<number>, prefix = "", isLeft = true) => {
   }
 };
 
-const data = [5, 10, 21, 87, 301, 350];
-const tree = new Tree();
-tree.buildTree(data);
+//const data = [5, 10, 21, 87, 301, 350];
+const tree = new Tree<number>();
+
+tree.insertRec(5);
+tree.insertRec(6);
+tree.insertRec(2);
+tree.insertRec(3);
+
+console.log(tree.find(6));
+
+//tree.buildTree(data);
 prettyPrint(tree.root as TreeNode<number>);
-console.log(tree);
